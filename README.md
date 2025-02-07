28366ed9a0a729e119641fb4c33cf5928396368e
Aleksandra Vir <galex97@mail.ru>
2025-02-07 23:28:29 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ajs-match
+[![Build status](https://ci.appveyor.com/api/projects/status/ivfyhvy0ia85lbtg?svg=true)](https://ci.appveyor.com/project/AleksVir/ajs-match)
---
+++ src/persons.js
@@ -0,0 +1,6 @@
+export default function sortByHealth(persons) {
+    const sortedPersons = persons.sort(function(a, b) {
+        return b.health - a.health;
+      });
+    return sortedPersons
+}
